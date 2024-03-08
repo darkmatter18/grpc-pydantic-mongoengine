53612c412e04c6485923647bd576fe718ab50c97
Arkadip Bhatttacharya <arkadip.bhattacharya@digite.com>
2024-03-08 17:16:00 +0530

    feat: added a robust rpc client

--- grpc_pydantic_mongoengine/rpc_client/base_rpc_client.py
+++ grpc_pydantic_mongoengine/rpc_client/base_rpc_client.py
@@ -11,8 +11,9 @@ from google.protobuf.json_format import MessageToDict
 from grpc_pydantic_mongoengine.proto.base import base_pb2
 
 # Protobuf Model Types
-CreateProtobufMessageType = TypeVar("CreateProtobufMessageType", bound=Message)
-UpdateProtobufMessageType = TypeVar("UpdateProtobufMessageType", bound=Message)
+CreateDataProtobufMessageType = TypeVar("CreateDataProtobufMessageType", bound=Message)
+UpdateDataProtobufMessageType = TypeVar("UpdateDataProtobufMessageType", bound=Message)
+UpdateQueryProtobufMessageType = TypeVar("UpdateQueryProtobufMessageType", bound=Message)
 SingleDataProtobufMessageType = TypeVar("SingleDataProtobufMessageType", bound=Message)
 MultiDataProtobufMessageType = TypeVar("MultiDataProtobufMessageType", bound=Message)
 
@@ -24,8 +25,9 @@ DataSchemaType = TypeVar("DataSchemaType", bound=BaseModel)
 
 class BaseRpcClient(
     Generic[
-        CreateProtobufMessageType,
-        UpdateProtobufMessageType,
+        CreateDataProtobufMessageType,
+        UpdateDataProtobufMessageType,
+        UpdateQueryProtobufMessageType,
         SingleDataProtobufMessageType,
         MultiDataProtobufMessageType,
         CreateSchemaType,
@@ -38,36 +40,37 @@ class BaseRpcClient(
         *,
         url: str,
         stub: object,
-        create_protobuf_message_class: Type[CreateProtobufMessageType],
-        update_query_protobuf_message_class: Type[UpdateProtobufMessageType],
-        single_protobuf_message_class: Type[SingleDataProtobufMessageType],
-        multi_protobuf_message_class: Type[MultiDataProtobufMessageType],
+        create_data_protobuf_message_class: Type[CreateDataProtobufMessageType],
+        update_data_protobuf_message_class: Type[UpdateDataProtobufMessageType],
+        update_query_protobuf_message_class: Type[UpdateQueryProtobufMessageType],
+        single_data_protobuf_message_class: Type[SingleDataProtobufMessageType],
+        multi_data_protobuf_message_class: Type[MultiDataProtobufMessageType],
         create_schema_class: Type[CreateSchemaType],
         update_schema_class: Type[UpdateSchemaType],
         data_schema_class: Type[DataSchemaType]
     ) -> None:
-        self.url = url
-        self.stub = stub
-        self.create_protobuf_message_class = create_protobuf_message_class
-        self.update_query_protobuf_message_class = update_query_protobuf_message_class
-        self.single_protobuf_message_class = single_protobuf_message_class
-        self.multi_protobuf_message_class = multi_protobuf_message_class
-        self.create_schema_class = create_schema_class
-        self.update_schema_class = update_schema_class
-        self.data_schema_class = data_schema_class
+        self._url = url
+        self._stub = stub
+        self._create_data_protobuf_message_class = create_data_protobuf_message_class
+        self._update_data_protobuf_message_class = update_data_protobuf_message_class
+        self._update_query_protobuf_message_class = update_query_protobuf_message_class
+        self._single_data_protobuf_message_class = single_data_protobuf_message_class
+        self._multi_data_protobuf_message_class = multi_data_protobuf_message_class
+        self._create_schema_class = create_schema_class
+        self._update_schema_class = update_schema_class
+        self._data_schema_class = data_schema_class
 
     async def create(
         self,
         *,
         obj_in: CreateSchemaType
     ) -> DataSchemaType:
-        print(obj_in.model_dump(mode="json"))
-        async with grpc.aio.insecure_channel(self.url) as channel:
-            stub = self.stub(channel)
-            resp: self.single_protobuf_message_class = await stub.Create(
-                self.create_protobuf_message_class(**obj_in.model_dump(mode="json"))
+        async with grpc.aio.insecure_channel(self._url) as channel:
+            stub = self._stub(channel)
+            resp: self._single_data_protobuf_message_class = await stub.Create(
+                self._create_data_protobuf_message_class(**obj_in.model_dump(mode="json"))
             )
-            return self.data_schema_class.model_validate(MessageToDict(
+            return self._data_schema_class.model_validate(MessageToDict(
                 resp,
                 use_integers_for_enums=False,
                 preserving_proto_field_name=True,
@@ -79,12 +82,12 @@ class BaseRpcClient(
         *,
         uuid: uuid.UUID
     ) -> DataSchemaType:
-        async with grpc.aio.insecure_channel(self.url) as channel:
-            stub = self.stub(channel)
-            resp: self.single_protobuf_message_class = await stub.GetByUUID(
+        async with grpc.aio.insecure_channel(self._url) as channel:
+            stub = self._stub(channel)
+            resp: self._single_data_protobuf_message_class = await stub.GetByUUID(
                 base_pb2.GetByUUIDMsg(uuid=str(uuid))
             )
-            return self.data_schema_class.model_validate(MessageToDict(
+            return self._data_schema_class.model_validate(MessageToDict(
                 resp,
                 use_integers_for_enums=False,
                 preserving_proto_field_name=True,
@@ -95,14 +98,14 @@ class BaseRpcClient(
         self,
         **query
     ) -> DataSchemaType:
-        async with grpc.aio.insecure_channel(self.url) as channel:
+        async with grpc.aio.insecure_channel(self._url) as channel:
             s = Struct()
             s.update(query)
-            stub = self.stub(channel)
-            resp: self.single_protobuf_message_class = await stub.Get(
+            stub = self._stub(channel)
+            resp: self._single_data_protobuf_message_class = await stub.Get(
                 blog=base_pb2.GetQuery(filter=s)
             )
-            return self.data_schema_class.model_validate(MessageToDict(
+            return self._data_schema_class.model_validate(MessageToDict(
                 resp,
                 use_integers_for_enums=False,
                 preserving_proto_field_name=True,
@@ -116,18 +119,18 @@ class BaseRpcClient(
         limit: int | None = None,
         **query,
     ) -> list[DataSchemaType]:
-        async with grpc.aio.insecure_channel(self.url) as channel:
+        async with grpc.aio.insecure_channel(self._url) as channel:
             s = Struct()
             s.update(query)
-            stub = self.stub(channel)
-            resp: self.multi_protobuf_message_class = await stub.GetMulti(
+            stub = self._stub(channel)
+            resp: self._multi_data_protobuf_message_class = await stub.GetMulti(
                 base_pb2.MultiGetQuery(
                     filter=s,
                     skip=skip,
                     limit=limit
                 )
             )
-            return TypeAdapter(list[self.data_schema_class]).validate_python(MessageToDict(
+            return TypeAdapter(list[self._data_schema_class]).validate_python(MessageToDict(
                 resp,
                 use_integers_for_enums=False,
                 preserving_proto_field_name=True,
@@ -142,17 +145,17 @@ class BaseRpcClient(
     ) -> DataSchemaType:
         s = Struct()
         s.update(query)
-        async with grpc.aio.insecure_channel(self.url) as channel:
-            stub = self.stub(channel)
-            resp: self.single_protobuf_message_class = await stub.Update(
-                self.update_query_protobuf_message_class(
-                    new_data=self.update_query_protobuf_message_class.new_data(
+        async with grpc.aio.insecure_channel(self._url) as channel:
+            stub = self._stub(channel)
+            resp: self._single_data_protobuf_message_class = await stub.Update(
+                self._update_query_protobuf_message_class(
+                    new_data=self._update_data_protobuf_message_class(
                         **new_data.model_dump(mode="json")
                     ),
                     filter=base_pb2.GetQuery(filter=s)
                 )
             )
-            return self.data_schema_class.model_validate(MessageToDict(
+            return self._data_schema_class.model_validate(MessageToDict(
                 resp,
                 use_integers_for_enums=False,
                 preserving_proto_field_name=True,
@@ -162,8 +165,8 @@ class BaseRpcClient(
     async def delete(self, **query):
         s = Struct()
         s.update(query)
-        async with grpc.aio.insecure_channel(self.url) as channel:
-            stub = self.stub(channel)
+        async with grpc.aio.insecure_channel(self._url) as channel:
+            stub = self._stub(channel)
             await stub.Delete(
                 base_pb2.GetQuery(filter=s)
             )
@@ -171,8 +174,8 @@ class BaseRpcClient(
     async def delete_many(self, **query):
         s = Struct()
         s.update(query)
-        async with grpc.aio.insecure_channel(self.url) as channel:
-            stub = self.stub(channel)
+        async with grpc.aio.insecure_channel(self._url) as channel:
+            stub = self._stub(channel)
             await stub.DeleteMulti(
                 base_pb2.MultiGetQuery(filter=s)
             )
